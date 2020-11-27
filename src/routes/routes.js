e42db70cf02fbae0ebced53808c19e3301d4bdab
unknown <dragon12xw@gmail.com>
2020-11-27 00:31:12 -0500

    server rutas ok

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -13,8 +13,8 @@ router.delete('/clientes/:id', deleteCliente)
 
 router.get('/reservas', getReservas)
 router.get('/reservas/:id',getReserva )
-//router.post('/reservas', postReserva)
-//router.put('/reservas/:id', putReserva)
-//router.delete('/reservas/:id', deleteReserva)
+router.post('/reservas', postReserva)
+router.put('/reservas/:id', putReserva)
+router.delete('/reservas/:id', deleteReserva)
 
 module.exports = router;
