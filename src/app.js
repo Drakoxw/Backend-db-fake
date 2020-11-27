2fb3010374801357f3c34d1fd683fd2b79749aae
unknown <dragon12xw@gmail.com>
2020-11-27 00:48:23 -0500

    server rutas ok+ cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
+
 //middlewares
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
